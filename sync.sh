f35c405a905bd5cb89cb1e6c8f2d0364c0582bf9
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-12-25 16:45:56 +0000

    clone the file at first place

--- sync.sh
+++ sync.sh
@@ -6,22 +6,22 @@
 case $rom in
                 1 )
                         mkdir -p ../rahul/ppui
-			cd ../rahul/ppui
-			cp clone.ch
-			repo init --depth=1 -u https://github.com/PixelPlusUI/official_manifest -b tiramisu
-			repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-			. clone.sh ;;
+                        cp clone.sh ../rahul/ppui
+                        cd ../rahul/ppui
+                        repo init --depth=1 -u https://github.com/PixelPlusUI/official_manifest -b tiramisu
+                        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+                        . clone.sh ;;
                 2 )
                         mkdir -p ../rahul/cherish
+                        cp clone.sh ../rahul/cherish
                         cd ../rahul/cherish
-                        cp clone.ch
-			repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu --depth=1
+                        repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu --depth=1
                         repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
                         . clone.sh ;;
                 3 )
                         mkdir -p ../rahul/evo
+                        cp clone.sh ../rahul/evo
                         cd ../rahul/evo
-                        cp clone.ch
                         repo init -u https://github.com/Evolution-X/manifest -b tiramisu --depth=1
                         repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
                         . clone.sh ;;

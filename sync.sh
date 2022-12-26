3f0112119c8030e036453c75b9d465db307bda84
Rahul Gorai <rahulgorai0206@gmail.com>
2022-12-26 12:09:15 +0530

    Reduce lines (Did dumb things before)

--- sync.sh
+++ sync.sh
@@ -8,24 +8,22 @@ case $rom in
                         mkdir -p ../rahul/ppui
                         cp clone.sh ../rahul/ppui
                         cd ../rahul/ppui
-                        repo init --depth=1 -u https://github.com/PixelPlusUI/official_manifest -b tiramisu
-                        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-                        . clone.sh ;;
+                        repo init -u https://github.com/PixelPlusUI/official_manifest -b tiramisu --depth=1 ;;
                 2 )
                         mkdir -p ../rahul/cherish
                         cp clone.sh ../rahul/cherish
                         cd ../rahul/cherish
-                        repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu --depth=1
-                        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-                        . clone.sh ;;
+                        repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu --depth=1 ;;
                 3 )
                         mkdir -p ../rahul/evo
                         cp clone.sh ../rahul/evo
                         cd ../rahul/evo
-                        repo init -u https://github.com/Evolution-X/manifest -b tiramisu --depth=1
-                        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-                        . clone.sh ;;
+                        repo init -u https://github.com/Evolution-X/manifest -b tiramisu --depth=1 ;;
                 * )
                         echo "Invalid Input "
                         exit ;;
                         esac
+
+
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. clone.sh
